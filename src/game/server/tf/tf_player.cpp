40a4b3d296badecdab07193d7b38a21ae0e12cda
unknown <danielmm8888@gmail.com>
2015-05-01 03:17:03 +0200

    Prevent people from joining the civilian and mercenary randomly

--- src/game/server/tf/tf_player.cpp
+++ src/game/server/tf/tf_player.cpp
@@ -1786,7 +1786,7 @@ void CTFPlayer::HandleCommand_JoinClass( const char *pClassName )
 	{
 		int i = 0;
 
-		for ( i = TF_CLASS_SCOUT ; i < TF_CLASS_COUNT_ALL ; i++ )
+		for ( i = TF_CLASS_SCOUT; i <= TF_CLASS_ENGINEER; i++ )
 		{
 			if ( stricmp( pClassName, GetPlayerClassData( i )->m_szClassName ) == 0 )
 			{
