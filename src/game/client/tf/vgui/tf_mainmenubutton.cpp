73d4813ed78838db19e6d9b57329ba0fae4e41cb
MrModez <zagirf@gmail.com>
2015-05-08 00:53:01 +0500

    MainMenuButton additions (border and text color)

--- src/game/client/tf/vgui/tf_mainmenubutton.cpp
+++ src/game/client/tf/vgui/tf_mainmenubutton.cpp
@@ -47,19 +47,25 @@ void CTFMainMenuButton::ApplySettings(KeyValues *inResourceData)
 {
 	BaseClass::ApplySettings(inResourceData);
 
-	Q_strncpy(pImageIdle, inResourceData->GetString("imageIdle", ""), sizeof(pImageIdle));
-	Q_strncpy(pImageHover, inResourceData->GetString("imageHover", ""), sizeof(pImageHover));
-	Q_strncpy(pImageClick, inResourceData->GetString("imageClick", ""), sizeof(pImageClick));
+	Q_strncpy(pDefaulImage, inResourceData->GetString("DefaulImage", ""), sizeof(pDefaulImage));
+	Q_strncpy(pArmedImage, inResourceData->GetString("ArmedImage", ""), sizeof(pArmedImage));
+	Q_strncpy(pDepressedImage, inResourceData->GetString("DepressedImage", ""), sizeof(pDepressedImage));
+
+	Q_strncpy(pDefaulBorder, inResourceData->GetString("DefaultBorder", ""), sizeof(pDefaulBorder));
+	Q_strncpy(pArmedBorder, inResourceData->GetString("ArmedBorder", ""), sizeof(pArmedBorder));
+	Q_strncpy(pDepressedBorder, inResourceData->GetString("DepressedBorder", ""), sizeof(pDepressedBorder));
+
+	Q_strncpy(pDefaulText, inResourceData->GetString("DefaultText", ""), sizeof(pDefaulText));
+	Q_strncpy(pArmedText, inResourceData->GetString("ArmedText", ""), sizeof(pArmedText));
+	Q_strncpy(pDepressedText, inResourceData->GetString("DepressedText", ""), sizeof(pDepressedText));
+
 	Q_strncpy(m_szCommand, inResourceData->GetString("command", ""), sizeof(m_szCommand));
 
 	m_bOnlyInGame = inResourceData->GetFloat("onlyingame", false);
 	m_bImageVisible = inResourceData->GetFloat("imagevisible", false);	
+	m_bBorderVisible = inResourceData->GetFloat("bordervisible", false);
 
-	pImage->SetImage(pImageIdle);
-	pButton->SetCommand(m_szCommand);
-
-	GetText(m_szText, sizeof(m_szText));
-	pButton->SetText(m_szText);
+	InvalidateLayout(false, true); // force ApplySchemeSettings to run
 }
 
 //-----------------------------------------------------------------------------
@@ -68,6 +74,12 @@ void CTFMainMenuButton::ApplySettings(KeyValues *inResourceData)
 void CTFMainMenuButton::ApplySchemeSettings(vgui::IScheme *pScheme)
 {
 	BaseClass::ApplySchemeSettings(pScheme);
+
+	pImage->SetImage(pDefaulImage);
+	pButton->SetCommand(m_szCommand);
+
+	GetText(m_szText, sizeof(m_szText));
+	pButton->SetText(m_szText);
 
 	pImage->SetVisible(m_bImageVisible);
 	pImage->SetEnabled(true);
@@ -91,10 +103,18 @@ void CTFMainMenuButton::ApplySchemeSettings(vgui::IScheme *pScheme)
 	SetDepressedColor(Color(0, 0, 0, 0), Color(0, 0, 0, 0));
 	SetSelectedColor(Color(0, 0, 0, 0), Color(0, 0, 0, 0));
 
-	pButton->SetDefaultColor(GetFgColor(), Color(0, 0, 0, 0));
-	pButton->SetArmedColor(GetFgColor(), Color(0, 0, 0, 0));
-	pButton->SetDepressedColor(GetFgColor(), Color(0, 0, 0, 0));
-	pButton->SetSelectedColor(GetFgColor(), Color(0, 0, 0, 0));
+	pButton->SetDefaultColor(pScheme->GetColor(pDefaulText, Color(255, 255, 255, 255)), Color(0, 0, 0, 0));
+	pButton->SetArmedColor(pScheme->GetColor(pArmedText, Color(255, 255, 255, 255)), Color(0, 0, 0, 0));
+	pButton->SetDepressedColor(pScheme->GetColor(pDepressedText, Color(255, 255, 255, 255)), Color(0, 0, 0, 0));
+	pButton->SetSelectedColor(pScheme->GetColor(pDepressedText, Color(255, 255, 255, 255)), Color(0, 0, 0, 0));
+
+	if (m_bBorderVisible)
+	{
+		pButton->SetDefaultBorder(pScheme->GetBorder(pDefaulBorder));
+		pButton->SetArmedBorder(pScheme->GetBorder(pArmedBorder));
+		pButton->SetDepressedBorder(pScheme->GetBorder(pDepressedBorder));
+		pButton->SetSelectedBorder(pScheme->GetBorder(pDepressedBorder));
+	}
 }
 
 //-----------------------------------------------------------------------------
@@ -102,7 +122,7 @@ void CTFMainMenuButton::ApplySchemeSettings(vgui::IScheme *pScheme)
 //-----------------------------------------------------------------------------
 void CTFMainMenuButton::SetDefaultAnimation()
 {
-	pImage->SetImage(pImageIdle);
+	pImage->SetImage(pDefaulImage);
 }
 
 
@@ -113,17 +133,18 @@ void CTFMainMenuButton::SendAnimation(MouseState flag)
 {
 	switch (flag)
 	{
+	//We can add additional stuff like animation here
 	case MOUSE_DEFAULT:
-		pImage->SetImage(pImageIdle);
+		pImage->SetImage(pDefaulImage);
 		break;
 	case MOUSE_ENTERED:
-		pImage->SetImage(pImageHover);
+		pImage->SetImage(pArmedImage);
 		break;
 	case MOUSE_EXITED:
-		pImage->SetImage(pImageIdle);
+		pImage->SetImage(pDefaulImage);
 		break;
 	case MOUSE_PRESSED:
-		pImage->SetImage(pImageClick);
+		pImage->SetImage(pDepressedImage);
 		break;
 	default:
 		break;
@@ -149,9 +170,59 @@ CTFButton::CTFButton(vgui::Panel *parent, const char *panelName, const char *tex
 void CTFButton::ApplySettings(KeyValues *inResourceData)
 {
 	BaseClass::ApplySettings(inResourceData);
-
-	m_flHoverTimeToWait = inResourceData->GetFloat("hover", -1);
+	//m_flHoverTimeToWait = inResourceData->GetFloat("hover", -1);
 }
+
+
+
+//-----------------------------------------------------------------------------
+// Purpose: Set armed button border attributes.
+//-----------------------------------------------------------------------------
+void CTFButton::SetArmedBorder(IBorder *border)
+{
+	_armedBorder = border;
+	InvalidateLayout(false);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Set selected button border attributes.
+//-----------------------------------------------------------------------------
+void CTFButton::SetSelectedBorder(IBorder *border)
+{
+	_selectedBorder = border;
+	InvalidateLayout(false);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CTFButton::ApplySchemeSettings(IScheme *pScheme)
+{
+	BaseClass::ApplySchemeSettings(pScheme);
+
+	_armedBorder = pScheme->GetBorder("ButtonArmedBorder");
+	_selectedBorder = pScheme->GetBorder("ButtonSelectedBorder");
+	InvalidateLayout();
+}
+
+
+//-----------------------------------------------------------------------------
+// Purpose: Get button border attributes.
+//-----------------------------------------------------------------------------
+IBorder *CTFButton::GetBorder(bool depressed, bool armed, bool selected, bool keyfocus)
+{
+	if (depressed)
+		return _depressedBorder;
+	if (armed)
+		return _armedBorder;
+	if (selected)
+		return _selectedBorder;
+
+	return _defaultBorder;
+}
+
+
+
 
 //-----------------------------------------------------------------------------
 // Purpose: 
@@ -212,7 +283,9 @@ void CTFButton::OnMouseReleased(vgui::MouseCode code)
 void CTFButton::SetMouseEnteredState(MouseState flag)
 {
 	iState = flag;
+	m_pParent->SendAnimation(flag);
 
+	/*
 	if (flag == MOUSE_ENTERED)
 	{
 		m_bMouseEntered = true;
@@ -225,7 +298,7 @@ void CTFButton::SetMouseEnteredState(MouseState flag)
 		{
 			m_flHoverTime = -1;
 		}
-		m_pParent->SendAnimation(flag);
+
 	}
 	else
 	{
@@ -234,4 +307,5 @@ void CTFButton::SetMouseEnteredState(MouseState flag)
 
 		m_pParent->SendAnimation(flag);
 	}
+	*/
 }
--- src/game/client/tf/vgui/tf_mainmenubutton.h
+++ src/game/client/tf/vgui/tf_mainmenubutton.h
@@ -15,6 +15,8 @@
 #include "tf_imagepanel.h"
 #include "tf_controls.h"
 
+using namespace vgui;
+
 class CTFButton;
 
 enum MouseState
@@ -43,13 +45,21 @@ public:
 	void SetDefaultAnimation();
 
 	bool OnlyInGame(void){ return m_bOnlyInGame; };
+	//void OnThink();
 
 private:
 	bool			m_bOnlyInGame;
 	bool			m_bImageVisible;
-	char			pImageIdle[64];
-	char			pImageHover[64];
-	char			pImageClick[64];
+	bool			m_bBorderVisible;
+	char			pDefaulImage[64];
+	char			pArmedImage[64];
+	char			pDepressedImage[64];
+	char			pDefaulBorder[64];
+	char			pArmedBorder[64];
+	char			pDepressedBorder[64];
+	char			pDefaulText[64];
+	char			pArmedText[64];
+	char			pDepressedText[64];
 	char			m_szCommand[64];
 	char			m_szText[64];
 	CTFImagePanel	*pImage;
@@ -70,6 +80,13 @@ public:
 	void SetParent(CTFMainMenuButton *m_pButton) { m_pParent = m_pButton; };
 	void ApplySettings(KeyValues *inResourceData);
 
+	// Set armed button border attributes.
+	virtual void SetArmedBorder(vgui::IBorder *border);
+	virtual void SetSelectedBorder(vgui::IBorder *border);
+	virtual void ApplySchemeSettings(IScheme *pScheme);
+	// Get button border attributes.
+	virtual IBorder *GetBorder(bool depressed, bool armed, bool selected, bool keyfocus);
+
 	void OnCursorExited();
 	void OnCursorEntered();
 	void OnMousePressed(vgui::MouseCode code);
@@ -79,11 +96,15 @@ private:
 	void SetMouseEnteredState(MouseState flag);
 
 private:
+	IBorder			  *_armedBorder;
+	IBorder			  *_selectedBorder;
+
 	float			m_flHoverTimeToWait;	// length of time to wait before reporting a "hover" message (-1 = no hover)
 	float			m_flHoverTime;			// when should a "hover" message be sent?
 	bool			m_bMouseEntered;		// used to track when the mouse is over a button
 	CTFMainMenuButton *m_pParent;
 	MouseState iState;
 };
+
 
 #endif // TF_MAINMENUBUTTON_H
