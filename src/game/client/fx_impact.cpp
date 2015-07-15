c5d96df37d087d765f9cedacb51fd8b7b218b14a
NicknineTheEagle <nik18.1995@gmail.com>
2015-07-15 19:50:09 +0300

    Disabled assert message

--- src/game/client/fx_impact.cpp
+++ src/game/client/fx_impact.cpp
@@ -286,8 +286,10 @@ static void PerformNewCustomEffects( const Vector &vecOrigin, trace_t &tr, const
 	VectorMultiply( shotDir, -1.0f, vecShotBackward );
 
 	Vector vecImpactPoint = ( tr.fraction != 1.0f ) ? tr.endpos : vecOrigin;
+	// Other games round it to nearest integer, so I guess we can afford skipping this check.
+#ifdef HL2_CLIENT_DLL
 	Assert( VectorsAreEqual( vecOrigin, tr.endpos, 1e-1 ) );
-
+#endif
 	SetImpactControlPoint( pEffect.GetObject(), 0, vecImpactPoint, tr.plane.normal, tr.m_pEnt ); 
 	SetImpactControlPoint( pEffect.GetObject(), 1, vecImpactPoint, vecReflect,		tr.m_pEnt ); 
 	SetImpactControlPoint( pEffect.GetObject(), 2, vecImpactPoint, vecShotBackward,	tr.m_pEnt ); 
