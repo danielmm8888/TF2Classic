e37c1c4e411c006f0f1849d09df210b787e69298
NicknineTheEagle <nik18.1995@gmail.com>
2015-08-10 20:18:25 +0300

    Enabled two-hander sapper draw anim

--- src/game/server/tf/tf_weapon_builder.cpp
+++ src/game/server/tf/tf_weapon_builder.cpp
@@ -145,9 +145,10 @@ bool CTFWeaponBuilder::Deploy( void )
 
 Activity CTFWeaponBuilder::GetDrawActivity( void )
 {
-	// sapper used to call different draw animations , one when invis and one when not.
-	// now you can go invis *while* deploying, so let's always use the one-handed deploy.
-	if ( GetType() == OBJ_ATTACHMENT_SAPPER )
+	CTFPlayer *pOwner = ToTFPlayer( GetOwner() );
+
+	// hax alert! use the one handed sapper deploy if we're invis
+	if ( pOwner && pOwner->m_Shared.InCond( TF_COND_STEALTHED ) && GetType() == OBJ_ATTACHMENT_SAPPER )
 	{
 		return ACT_VM_DRAW_DEPLOYED;
 	}
