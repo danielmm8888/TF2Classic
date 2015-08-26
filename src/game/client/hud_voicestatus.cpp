58f084c99d198daf06c7e2a3c2a9022fcf6f1208
MrModez <zagirf@gmail.com>
2015-08-27 03:16:18 +0500

    Colored voice panel

--- src/game/client/hud_voicestatus.cpp
+++ src/game/client/hud_voicestatus.cpp
@@ -16,6 +16,11 @@
 #include "voice_common.h"
 #include "vgui_avatarimage.h"
 
+#if defined (TF_CLASSIC_CLIENT)
+#include "tf_gamerules.h"
+#include "c_tf_playerresource.h"
+#endif
+
 ConVar *sv_alltalk = NULL;
 
 //=============================================================================
@@ -355,7 +360,21 @@ void CHudVoiceStatus::Paint()
 		float oldAlphaMultiplier = surface()->DrawGetAlphaMultiplier();
 		surface()->DrawSetAlphaMultiplier(oldAlphaMultiplier * m_SpeakingList[i].fAlpha);
 
+#if defined(TF_CLASSIC_CLIENT)
+		Color c;
+
+		if (TFGameRules() && TFGameRules()->IsDeathmatch())
+		{
+			C_TF_PlayerResource *tf_PR = dynamic_cast<C_TF_PlayerResource *>(g_PR);
+			c = tf_PR->GetPlayerColor(playerId);
+		}
+		else
+		{
+			c = g_PR->GetTeamColor(g_PR ? g_PR->GetTeam(playerId) : TEAM_UNASSIGNED);
+		}
+#else
 		Color c = g_PR->GetTeamColor( g_PR ? g_PR->GetTeam(playerId) : TEAM_UNASSIGNED );
+#endif
 
 		c[3] = 128;
 
