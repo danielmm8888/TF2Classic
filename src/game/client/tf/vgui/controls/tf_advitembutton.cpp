690d0d9c09cfc5c31536533051d2cacae3a14273
MrModez <zagirf@gmail.com>
2015-12-07 05:00:42 +0500

    Added tooltip with the item info

---
+++ src/game/client/tf/vgui/controls/tf_advitembutton.cpp
@@ -0,0 +1,86 @@
+﻿#include "cbase.h"
+#include "tf_advitembutton.h"
+#include "vgui_controls/Frame.h"
+#include <vgui/ISurface.h>
+#include <vgui/IVGui.h>
+#include <vgui/IInput.h>
+#include "vgui_controls/Button.h"
+#include "vgui_controls/ImagePanel.h"
+#include "tf_controls.h"
+#include <filesystem.h>
+#include <vgui_controls/AnimationController.h>
+#include "basemodelpanel.h"
+#include "panels/tf_dialogpanelbase.h"
+#include "inputsystem/iinputsystem.h"
+
+using namespace vgui;
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+DECLARE_BUILD_FACTORY_DEFAULT_TEXT(CTFAdvItemButton, CTFAdvItemButtonBase);
+
+//-----------------------------------------------------------------------------
+// Purpose: Constructor
+//-----------------------------------------------------------------------------
+CTFAdvItemButton::CTFAdvItemButton(vgui::Panel *parent, const char *panelName, const char *text) : CTFAdvButton(parent, panelName, text)
+{
+	Init();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Destructor
+//-----------------------------------------------------------------------------
+CTFAdvItemButton::~CTFAdvItemButton()
+{
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CTFAdvItemButton::Init()
+{
+	BaseClass::Init();
+	pItemDefinition = NULL;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void CTFAdvItemButton::SendAnimation(MouseState flag)
+{
+	BaseClass::SendAnimation(flag);
+	switch (flag)
+	{
+	case MOUSE_DEFAULT:
+		if (pItemDefinition)
+			MAINMENU_ROOT->ShowItemToolTip(pItemDefinition);
+		break;
+	case MOUSE_ENTERED:
+		if (pItemDefinition)
+			MAINMENU_ROOT->ShowItemToolTip(pItemDefinition);
+		break;
+	case MOUSE_EXITED:
+		if (pItemDefinition)
+			MAINMENU_ROOT->HideItemToolTip();
+		break;
+	case MOUSE_PRESSED:
+		break;
+	default:
+		break;
+	}
+}
+
+void CTFAdvItemButton::SetItemDefinition(EconItemDefinition *pItemData)
+{
+	pItemDefinition = pItemData;
+
+	char szIcon[128];
+	Q_snprintf(szIcon, sizeof(szIcon), "../%s_large", pItemData->image_inventory);
+	SetImage(szIcon);
+
+
+	char szWeaponName[32];
+	Q_snprintf(szWeaponName, sizeof(szWeaponName), "%s", pItemData->item_name);
+	SetText(szWeaponName);
+}
---
+++ src/game/client/tf/vgui/controls/tf_advitembutton.h
@@ -0,0 +1,36 @@
+﻿#ifndef TF_ADVITEMBUTTON_H
+#define TF_ADVITEMBUTTON_H
+#ifdef _WIN32
+#pragma once
+#endif
+
+#include <vgui_controls/EditablePanel.h>
+#include "tf_imagepanel.h"
+#include "tf_advbutton.h"
+
+using namespace vgui;
+
+class CTFButton;
+#define pImageColorSelected		(!m_bSelected ? pImageColorDefault : pSelectedColor) 
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+class CTFAdvItemButton : public CTFAdvButton
+{
+	friend class CTFButton;
+public:
+	DECLARE_CLASS_SIMPLE(CTFAdvItemButton, CTFAdvButton);
+
+	CTFAdvItemButton(vgui::Panel *parent, const char *panelName, const char *text);
+	~CTFAdvItemButton();
+	void Init();
+	void SendAnimation(MouseState flag);
+	void SetItemDefinition(EconItemDefinition *pItemData);
+
+protected:
+	EconItemDefinition *pItemDefinition;
+};
+
+
+#endif // TF_ADVITEMBUTTON_H
