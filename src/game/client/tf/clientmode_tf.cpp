4b50f19bdf99a655c896bc916af1107b34a8e06c
NicknineTheEagle <nik18.1995@gmail.com>
2016-06-25 03:10:17 +0300

    Changed default FOV to 90

--- src/game/client/tf/clientmode_tf.cpp
+++ src/game/client/tf/clientmode_tf.cpp
@@ -52,7 +52,7 @@
 #endif
 
 ConVar default_fov( "default_fov", "75", FCVAR_CHEAT );
-ConVar fov_desired( "fov_desired", "75", FCVAR_ARCHIVE | FCVAR_USERINFO, "Sets the base field-of-view.", true, 75.0, true, 100.0 );
+ConVar fov_desired( "fov_desired", "90", FCVAR_ARCHIVE | FCVAR_USERINFO, "Sets the base field-of-view.", true, 75.0, true, MAX_FOV );
 
 void HUDMinModeChangedCallBack( IConVar *var, const char *pOldString, float flOldValue )
 {
